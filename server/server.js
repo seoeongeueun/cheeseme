f5f5e67580b10c0dc37b915f93e3ce7483a23ce3
seoeongeueun <erin9901@gmail.com>
2022-05-03 23:31:58 -0400

    update gitignore
    ignore node_modules

--- server/server.js
+++ server/server.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log('connected to db')
+}).catch( err => {
+    console.log(err.message);
+});
 
 const app = express();
 
@@ -6,7 +16,7 @@ app.get('/api/products', (req, res) => {
     res.send()
 })
 
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
     console.log(`server at http://localhost:${port}`)
