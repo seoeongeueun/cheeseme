ffe00b7daa96f0be80b7464c89314bed3abc3faa
seoeongeueun <92116454+seoeongeueun@users.noreply.github.com>
2022-08-04 02:17:08 -0400

    add function to todo list
    + button adds goals to todo list

--- client/src/widgets/Todo.js
+++ client/src/widgets/Todo.js
@@ -1,18 +1,41 @@
 import { useEffect, useState } from 'react'
+import AddRoundedIcon from '@mui/icons-material/AddRounded';
+import RemoveRoundedIcon from '@mui/icons-material/RemoveRounded';
 
 function Todo(){
+    const [count, setCount] = useState(1);
+    const [goals, setGoals] = useState(['cse323', 'eat']);
+
+    useEffect(() => {
+
+    }, [goals, count])
+
+    const handleAddTodo = () => {
+        goals.push('')
+        setCount(count+1)
+    }
+
+    const handleEditTodo = (key, value) => {
+        goals[key] = value;
+    }
+
+    const handleKeyPress = (event) => {
+        if(event.key === 'Enter'){
+          setCount(count+1)
+        }
+    }
+
     return (
         <div className="todoWidget">
-            <p1>To Do</p1>
+            <div className="todoHeader">
+                <p>To Do</p>
+                <button onClick={handleAddTodo}><AddRoundedIcon sx={{size: '20px'}}/></button>
+            </div>
             <div className="todoList">
-                <div className="checkboxButton">
-                    <input type="checkbox" name="check1"/>
-                    <label>Study cse 323</label>
-                </div>
-                <div className="checkboxButton">
-                    <input type="checkbox" name="check2"/>
-                    <label>Test todo 2</label>
-                </div>
+                {goals.map((value, key) => (<div className="checkboxButton">
+                    <input type="checkbox" name={key}/>
+                    {value==='' ? <input onChange={(e) => handleEditTodo(key, e.target.value)} onKeyPress={handleKeyPress}/> : <label>{value}</label>}
+                </div>))}
             </div>
         </div>
     )
