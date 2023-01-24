61cf5078eab58e25275575feef1f3e0c87515261
seoeongeueun <92116454+seoeongeueun@users.noreply.github.com>
2023-01-24 23:58:36 +0900

    onchange value calendar

--- client/src/widgets/Calendar.js
+++ client/src/widgets/Calendar.js
@@ -3,10 +3,16 @@ import Calendar from 'react-calendar';
 import OpenWithSharpIcon from '@mui/icons-material/OpenWithSharp';
 
 function CalendarWidget(props){
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        console.log("clicked date: ", date.getTime())
+    }, [date])
+
     return (
         <div className="calendarWidget">
             {props.move && <strong><OpenWithSharpIcon sx={{fontSize: '7rem'}}/></strong>}
-            <Calendar />
+            <Calendar onChange={setDate} value={date}/>
         </div>
     )
 
