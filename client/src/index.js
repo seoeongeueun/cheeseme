1dcc2502241ca62545d3a03909a9509ce3880efb
seoeongeueun <erin9901@gmail.com>
2023-05-15 18:18:49 +0900

    react strictmode

--- client/src/index.js
+++ client/src/index.js
@@ -11,13 +11,15 @@ const store = createStore(rootReducer);
 
 //const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
