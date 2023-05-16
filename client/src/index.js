02c70cc38e0f62836655d00444cbb980d7343099
seoeongeueun <erin9901@gmail.com>
2023-05-16 11:23:21 +0900

    strictmode off

--- client/src/index.js
+++ client/src/index.js
@@ -11,11 +11,9 @@ const store = createStore(rootReducer);
 
 //const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>
-  </React.StrictMode>,
+    </Provider>,
   document.getElementById('root')
 );
 
