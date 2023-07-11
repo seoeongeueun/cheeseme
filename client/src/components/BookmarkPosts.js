81481e805eeab1a980d470cda83bb20f51057302
seoeongeueun <erin9901@gmail.com>
2023-07-11 23:27:11 +0900

    bookmark page color add

--- client/src/components/BookmarkPosts.js
+++ client/src/components/BookmarkPosts.js
@@ -3,11 +3,14 @@ import { useEffect, useState } from 'react';
 import Highlighter from "react-highlight-words";
 import { useSelector, useDispatch } from 'react-redux';
 import { changeDate } from '../modules/datePick';
+import GridLines from 'react-gridlines';
 
 
 function BookmarkPosts(props){
     const [foundPosts, setFoundPosts] = useState([]);
     const [clicked, setClicked] = useState();
+    const colorCode = ['rgba(253, 223, 126, 0.3)', 'rgba(103, 235, 250, 0.3)', 'rgba(250, 169, 157, 0.3)', 'rgba(155, 251, 225, 0.3)', 'rgba(206, 151, 251, 0.3)'];
+
     const dispatch = useDispatch();
     const onChangeDate = d => dispatch(changeDate(d));
     const instance = axios.create({
@@ -56,22 +59,24 @@ function BookmarkPosts(props){
     };
 
     return (
-        <div className='bookmarkInnerBorder'>
-            <div className="rightBookmarkPosts">
-                <p style={{textAlign: "center"}}>Bookmarked Posts</p>
-                <div className='foundPostsCategory'>
-                    {foundPosts?.length > 0 ? foundPosts.map((p) => (
-                        <div className='foundSearchItem' onClick={() => handleClick(p.date)}>
-                            <span className='foundDate'>{new Date(p.date).getMonth()+1}.{new Date(p.date).getDate()}.{new Date(p.date).getFullYear()}</span>
-                            <div className='foundContent'>
-                                {p.title.length <= 0 ? <span className='foundContentTitle'>No Title</span> : <span className='foundContentTitle'>{p.title}</span>}
-                                <span style={{marginLeft: '3rem'}}>{removeTags(p.text).substring(0, 200).length >= 200 ? removeTags(p.text).substring(0, 200) + '...' : removeTags(p.text).substring(0, 200)}</span>
+        <GridLines className="grid-area" cellWidth={60} strokeWidth={1} strokeWidth2={1} cellWidth2={12} lineColor2={"#eeeeee"} lineColor={"#d9d9d9"}>
+            <div className='bookmarkInnerBorder'> 
+                <div className="rightBookmarkPosts">
+                    <p style={{textAlign: "center", fontSize: '3rem'}}>Bookmarked Posts</p>
+                    <div className='foundPostsCategory'>
+                        {foundPosts?.length > 0 ? foundPosts.map((p, index) => (
+                            <div className='foundSearchItem' onClick={() => handleClick(p.date)}>
+                                <span className='foundDate'>{new Date(p.date).getMonth()+1}.{new Date(p.date).getDate()}.{new Date(p.date).getFullYear()}</span>
+                                <div className='foundContent'>
+                                    {p.title.length <= 0 ? <span className='foundContentTitle' style={{width: 'max-content', backgroundColor: colorCode[index%colorCode.length]}}>No Title</span> : <span className='foundContentTitle' style={{width: 'max-content', backgroundColor: colorCode[index%colorCode.length]}}>{p.title}</span>}
+                                    <span style={{marginLeft: '3rem'}}>{removeTags(p.text).substring(0, 200).length >= 200 ? removeTags(p.text).substring(0, 200) + '...' : removeTags(p.text).substring(0, 200)}</span>
+                                </div>
                             </div>
-                        </div>
-                    )) : <div className='foundSearchItemNotFound'>{props.userId ? <span>No Bookmarked Posts Yet</span> : <span>Login Required</span>}</div>}
+                        )) : <div className='foundSearchItemNotFound'>{props.userId ? <span>No Bookmarked Posts Yet</span> : <span>Login Required</span>}</div>}
+                    </div>
                 </div>
             </div>
-        </div>
+        </GridLines>
     )
 }
 
